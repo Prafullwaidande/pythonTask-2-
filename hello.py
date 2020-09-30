466d584966c8f59022f21f70ee4931544ff78b20
PrafullWaidande <64540814+Prafullwaidande@users.noreply.github.com>
2020-10-01 04:16:23 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,24 +4,27 @@ import pyttsx3
 
 print("***************************************       Welcome To My Tools       ***************************************")
 pyttsx3.speak("welcome to my tools, How can i help you ")
-	
-print("Tell Your requirements : ", end='')
-# ch = input()
 
-r = sr.Recognizer()
+while True:	
+	print("Tell Your requirements : ", end='')
+	# ch = input()
 
-with sr.Microphone() as source:
+	r = sr.Recognizer()
+
+	with sr.Microphone() as source:
    		print('start say ..')
    		audio = r.listen(source)
-print('we got it, speech done ..')
+	print('we got it, speech done ..')
 
-ch = r.recognize_google(audio)
+	ch = r.recognize_google(audio)
 
-if ("date" in ch) and (("run" in ch) or ("execute" in ch)or("open" in ch)):
+	if ("date" in ch) and (("run" in ch) or ("execute" in ch)or("open" in ch)):
 	   	webbrowser.open("http://192.168.1.103/cgi-bin/cmd.py?c=date")
-elif ("docker" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
+	elif ("docker" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
 	        webbrowser.open("http://192.168.1.103/drun.html")
-elif ("redhat" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
+	elif ("redhat" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
    		webbrowser.open("http://192.168.1.103/home.html")
-else:
- print("not understand")
+	elif ("exit" in ch) or ("quit" in ch):
+		break
+	else:
+ 	   print("not understand")
