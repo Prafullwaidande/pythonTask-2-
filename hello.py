8cb03eeb1330da5bdfc11462d6a5775ca84b07f7
PrafullWaidande <64540814+Prafullwaidande@users.noreply.github.com>
2020-10-01 02:40:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,23 +2,26 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 
-print("Welcome to my Tools")
-pyttsx3.speak("welcome to my tools")
-print("Enter ur requirements : ", end='')
+print("***************************************       Welcome To My Tools       ***************************************")
+pyttsx3.speak("welcome to my tools, How can i help you ")
+	
+print("Tell Your requirements : ", end='')
 # ch = input()
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-   print('start say ..')
-   audio = r.listen(source)
-   print('we got it, speech done ..')
+   		print('start say ..')
+   		audio = r.listen(source)
+print('we got it, speech done ..')
 
 ch = r.recognize_google(audio)
 
 if ("date" in ch) and (("run" in ch) or ("execute" in ch)or("open" in ch)):
-   webbrowser.open("http://192.168.1.103/cgi-bin/cmd.py?c=date")
-elif ("calendar" in ch) and (("run" in ch) or ("execute" in ch)):
-   webbrowser.open("http://192.168.1.103/cgi-bin/cmd.py?c=cal")
+	   	webbrowser.open("http://192.168.1.103/cgi-bin/cmd.py?c=date")
+elif ("docker" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
+	        webbrowser.open("http://192.168.1.103/drun.html")
+elif ("redhat" in ch) and (("run" in ch) or ("execute" in ch) or ("open" in ch)):
+   		webbrowser.open("http://192.168.1.103/home.html")
 else:
-   print("not understand")
+ print("not understand")
