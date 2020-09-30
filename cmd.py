c3e95f3e4d08185ac853e45e5d55129d6850c755
PrafullWaidande <64540814+Prafullwaidande@users.noreply.github.com>
2020-09-30 20:20:45 +0530

    Create cmd.py

---
+++ cmd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print("content-type: text/html\n")
+print()
+
+import cgi
+import subprocess
+
+form = cgi.FieldStorage()
+cmd = form.getvalue("c")
+
+output = subprocess.getoutput(cmd)
+print(output)
